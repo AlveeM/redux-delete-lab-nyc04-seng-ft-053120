f3bae1b00c3d94a0876c1dcdd52071ed519a40fd
AlveeM <alvee.akand@outlook.com>
2020-08-30 23:36:30 -0400

    Done.

--- src/components/Band.js
+++ src/components/Band.js
@@ -3,9 +3,11 @@ import React, { Component } from 'react';
 class Band extends Component {
 
   render() {
+    const band = this.props.band;
     return(
       <div>
-        Band Component
+        <li key={band.id}>{band.name}</li>
+        <button onClick={() => this.props.deleteBand(band.id)}>Delete</button>
       </div>
     );
   }
